c907310b1f7d26a92b0e2a29f3685475f96ca425
Ajith SB <asthammanth@opentrends.net>
2024-02-12 22:54:01 +0530

    Adding initial files

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Aj on 12/02/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
